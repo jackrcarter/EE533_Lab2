137fecb4c4093a605aca24e9c213992cef7260af
Jack Carter <71847270+jackrcarter@users.noreply.github.com>
2025-01-27 22:42:39 -0800

    Post Demo Updates
    Further developed tb file for completeness, updated a typo in top file (shifter_result vs shifter_results)

---
+++ Part_C/alu_32_top.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:24:09 01/25/2025 
+// Design Name: 
+// Module Name:    alu_32_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu_32_top(
+   // Inputs
+	input [31:0] a,
+	input [31:0] b,
+	input [2:0] op,
+	input cin,
+	
+	
+	// Outputs
+	output [31:0] o,
+	output eq,
+	output neg,
+	output cout
+);
+
+	// ADD   = 000
+	// SUB   = 001
+	// SHIFT = 010
+	// OR    = 011
+	// EQUAL = 100
+
+	wire [31:0] adder_wire;
+	wire adder_carry;
+	wire [31:0] subtractor_wire;
+	wire subtractor_carry;
+	wire [31:0] shifter_result;
+	wire shifter_carry;
+	wire [31:0] bitwise_or_result;
+	wire eq_mid;
+	// Assigns
+	adder_32 adder (.cin(cin), .a(a), .b(b), .sum(adder_wire), .cout(adder_carry));
+	subtractor_32 subtractor (.a(a), .b(b), .bin(cin), .d(subtractor_wire), .bout(subtractor_carry));
+	bitshift_left_32 shifter (.a(a), .cin(cin), .b(shifter_result), .cout(shifter_carry));
+	bitwise_or_32 bitwise_or (.a(a), .b(b), .c(bitwise_or_result));
+	equality_32 equality_check (.a(a), .b(b), .eq(eq_mid));
+	
+	mux4_to_1_32bit output_mux (.a(adder_wire), .b(subtractor_wire), .c(shifter_result), .d(bitwise_or_result), .sel(op[1:0]), .o(o));
+	
+	// Need to do the mux for cout
+	wire cout_mux1;
+	mux_2_to_1 cout_mux (.a(adder_carry), .b(subtractor_carry), .sel(op[0]), .c(cout_mux1));
+	mux_2_to_1 final_mux (.a(cout_mux1), .b(shifter_carry), .sel(op[1]), .c(cout));
+	
+	assign eq =(op == 3'b100) ? eq_mid : 0;
+	assign neg = (op == 3'b001) ? (subtractor_carry) : 0;
+	
+endmodule
--- Part_C/alu_32_top_tb.v
+++ Part_C/alu_32_top_tb.v
@@ -36,7 +36,7 @@ module alu_32_top_tb;
 		.a(a),
 		.b(b),
 		.op(op),
-		.cin(.cin),
+		.cin(cin),
 		.o(o),
 		.eq(eq),
 		.neg(neg),
@@ -52,26 +52,90 @@ module alu_32_top_tb;
 		cin = 1'b0;
 		#100;
 		
-		// Test 1
-		a = 32'd5;
-		b = 32'd10;
+		// Test 1: Addition: 
+		a = 32'd1;
+		b = 32'd2;
 		op = 3'd0;
 		cin = 1'b0;
 		#100;
 		
-		// Test 2
-		a = 32'd20;
-		b = 32'd19;
-		op = 3'd1;
+		// Test 2: Addition w/ Cin:
+		a = 32'd1;
+		b = 32'd2;
+		op = 3'd0;
 		cin = 1'b1;
 		#100;
 		
-		// Test 2
-		a = 32'd5;
-		b = 32'd19;
-		op = 3'd1;
+		// Test 3: Addition w/ Cout:
+		a = 32'hFFFFFFFF;
+		b = 32'h00000001;
+		op = 3'd0;
+		cin = 1'b0;
+		#100;
+		
+		// Test 4: Subtraction:
+		a = 32'd10;
+		b = 32'd8;
+		op = 3'b001;
+		cin = 1'b0;
+		#100;
+		
+		// Test 5: Subtraction w/ Cin:
+		a = 32'd10;
+		b = 32'd8;
+		op = 3'b001;
 		cin = 1'b1;
 		#100;
+		
+		// Test 6: Subtraction w/ neg:
+		a = 32'd10;
+		b = 32'd12;
+		op = 3'b001;
+		cin = 1'b0;
+		#100;
+		
+		// Test 7: Shift left:
+		a = 32'h000F000F;
+		b = 32'd00;
+		op = 3'b010;
+		cin = 1'b0;
+		#100;
+		
+		// Test 8: Shift left w/ Cin:
+		a = 32'h000F000F;
+		b = 32'd00;
+		op = 3'b010;
+		cin = 1'b1;
+		#100;
+		
+		// Test 9: Shift left w/ Cout:
+		a = 32'hF00F000F;
+		b = 32'd00;
+		op = 3'b010;
+		cin = 1'b1;
+		#100;
+		
+		// Test 10: Or-bitwise:
+		a = 32'hF00F000F;
+		b = 32'h0FF0FFF0;
+		op = 3'b011;
+		cin = 1'b0;
+		#100;
+		
+		// Test 11: Equality Not Equal:
+		a = 32'hF00F000F;
+		b = 32'h0FF0FFF0;
+		op = 3'b100;
+		cin = 1'b0;
+		#100;
+		
+		// Test 12: Equality Equal:
+		a = 32'h0FF0FFF0;
+		b = 32'h0FF0FFF0;
+		op = 3'b100;
+		cin = 1'b0;
+		#100;
+
 		
 		
 		
